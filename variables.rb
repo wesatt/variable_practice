575c94ecd7c6e7fa955e392d8fa63993a13c69a6
Wes Garcia <wes@alltradestech.com>
2022-02-07 19:42:43 -0800

    Add annotation

--- variables.rb
+++ variables.rb
@@ -1,15 +1,26 @@
+# variable name is declared and assigned to the string "Wes"
 name = "Wes"
+# variable name city is declared and assigned to the string "Los Angeles"
 city = "Los Angeles"
+# variable name favorite_season is declared and assigned to the string "Autumn"
 favorite_season = "Autumn"
 
+# variable favorite_number is declared and assigned to the integer 13
 favorite_number = 13
+# variable age is declared and assigned to the integer 36
 age = 36
 
+# variable books_read is declared and assigned to the float 86.7
 books_read = 86.7
 
+# variable having_fun is declared and assigned to the boolean true
 having_fun = true
+# vriable feeling_hungry is declared and assigned to the value false
 feeling_hungry = false
 
+# variable name is reassigned to add "Mr." to the front of the original string variable
 name = "Mr." + name
+# variable city is reassigned to add " , CA" to the end of the original string variable
 city = city + ", CA"
+# variable favorite_season is reassigned to add " or Fall" to the end of the original string variable
 favorite_season = favorite_season + " or Fall"
